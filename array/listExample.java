47506adbd2ea4cdbf0d96efccd44637eacae6e87
Bibash Thapa <bibashthapa1432@gmail.com>
2026-08-18 07:43:07 +0545

    Add listExample class to demonstrate usage of ArrayList in Java

---
+++ array/listExample.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class listExample {
+  public static void main(String[] args)
+  {
+    List<Integer> l1= new ArrayList<>();
+    l1.add(5);
+    l1.add(6);
+    l1.add(7);
+    l1.add(8);
+    System.out.println(l1);
+    int i=l1.size();
+    System.out.println("the size is "+i);
+    
+
+
+
+
+  }
+
+
+  
+}
