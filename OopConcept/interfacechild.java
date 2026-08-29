8959d23fe96e2d872d31f218dea95189c95fb3ff
Bibash Thapa <bibashthapa1432@gmail.com>
2026-08-29 22:04:18 +0545

    Add missing @Override annotations in interfacechild class methods

--- OopConcept/interfacechild.java
+++ OopConcept/interfacechild.java
@@ -1,9 +1,11 @@
 public class interfacechild implements interfaceExample{
+    @Override
     public void sum() {
         int sum = 5 + 7;
         System.out.println("The sum is: " + sum);
     }
 
+    @Override
     public void sub() {
         int sub = 10 - 5;
         System.out.println("The subtraction is: " + sub);
